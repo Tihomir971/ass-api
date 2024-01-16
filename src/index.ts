6ae2e55a98028dac7a05dfc8877da372bb8d7eab
Tihomir971 <tihomir.jev@outlook.com>
2024-01-16 10:09:49 +0100

    type ParseFunctions

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,18 @@ interface MyRequestBody {
 	barcode: string;
 }
 type ParseFunctions = {
-	[key: string]: (barcode: string) => Promise<string | undefined>;
+	[key: string]: (barcode: string | undefined) => Promise<Response>;
 };
+let source: string | undefined = undefined;
+
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
 		if (request.method === 'POST') {
 			const body: MyRequestBody = await request.json();
-			const source = body.source;
+			source = body.source;
 			const barcode = body.barcode;
 			let href = 'Undefined';
+
 			if (source && barcode) {
 				const browser = await puppeteer.launch(env.MYBROWSER);
 				const page = await browser.newPage();
@@ -51,34 +54,40 @@ export default {
 };
 
 const vendorPrice: ParseFunctions = {
-	cenoteka: async function (barcode: string) {
-		let href = 'Undefined';
-		let browser;
-		try {
-			browser = await puppeteer.launch(env.MYBROWSER);
-			const page = await browser.newPage();
-			await page.goto('https://cenoteka.rs/');
-			href = 'Goto';
-			// Wait for suggest overlay to appear and click "show all results".
-			/* const homeSearchSelector = '.nav_search_input__CB_KM';
-			await page.waitForSelector(homeSearchSelector);
-			await page.click(homeSearchSelector); */
-
-			// Wait for the results page to load and display the results.
-			/* 			const innerSearchSelector = '#nav-search';
-			await page.waitForSelector(innerSearchSelector);
-			await page.type(innerSearchSelector, barcode); */
-
-			/* 			const searchResultSelector =
-				'#__next > div > form > div.search_search_content_wrap__Ab4ZA.container > div.row.pt-4.pb-1 > div > div > a';
-			await page.waitForSelector(searchResultSelector, { timeout: 5000 });
-			href = await page.$eval(searchResultSelector, (elm) => elm.href); */
-		} catch (error) {
-			console.log('Error show', error);
-			//return href;
-		} finally {
-			await browser?.close();
+	cenoteka: async function (barcode: string | undefined) {
+		if (barcode) {
+			let href = 'Undefined';
+			let browser;
+			try {
+				browser = await puppeteer.launch(env.MYBROWSER);
+				const page = await browser.newPage();
+				await page.goto('https://cenoteka.rs/');
+				// Wait for suggest overlay to appear and click "show all results".
+				const homeSearchSelector = '.nav_search_input__CB_KM';
+				await page.waitForSelector(homeSearchSelector);
+				await page.click(homeSearchSelector);
+				// Wait for the results page to load and display the results.
+				const innerSearchSelector = '#nav-search';
+				await page.waitForSelector(innerSearchSelector);
+				await page.type(innerSearchSelector, barcode);
+				// This code waits for a specific webpage element to load within 5 seconds, then retrieves and stores its URL.
+				const searchResultSelector =
+					'#__next > div > form > div.search_search_content_wrap__Ab4ZA.container > div.row.pt-4.pb-1 > div > div > a';
+				await page.waitForSelector(searchResultSelector, { timeout: 5000 });
+				href = await page.$eval(searchResultSelector, (elm) => elm.href);
+				// Create a new object with the barcode
+				const responseBody = { path: href, source: source };
+				// Convert the object to a JSON string
+				const responseJson = JSON.stringify(responseBody);
+				// Return the JSON string as the response
+				return new Response(responseJson, { status: 200, headers: { 'Content-Type': 'application/json' } });
+			} catch (error) {
+				console.log('Error show', error);
+				//return href;
+			} finally {
+				await browser?.close();
+			}
 		}
-		return href;
+		return new Response('Shit i didnt find', { status: 404, headers: { 'Content-Type': 'application/json' } });
 	},
 };
